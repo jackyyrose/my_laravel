6258c4e9d714bd417b5cae56bf88658890775e88
jack <jack.xu.nz@gmail.com>
2019-12-17 01:20:52 +0000

    header and footer blade

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <img class="brand-icon" src="https://mpng.pngfly.com/20180920/hq/kisspng-laravel-software-framework-web-framework-php-zend-laravel-software-framework-php-web-framework-model-5ba3437deb19e7.104986071537426301963.jpg" >
+    <a href="https://laravel-china.org/courses" target=_blank>
+        every day a course
+    </a>
+    <div class="float-right">
+    <a href="/about" >ABOUT</a>
+    </div>
+</footer>
