a978749d900a419aa310e4c96be0c0f535ad3541
jack <jack.xu.nz@gmail.com>
2019-12-17 03:07:55 +0000

    user signup page

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container ">
-        <a class="navbar-brand" href="{{route('home')}}">Weibo App</a>
+        <a class="navbar-brand" href="{{route('home')}}">DATA App</a>
         <ul class="navbar-nav justify-content-end">
             <li class="nav-item"><a class="nav-link" href="{{route('help')}}">HELP</a></li>
             <li class="nav-item" ><a class="nav-link" href="#">LOGIN</a></li>
