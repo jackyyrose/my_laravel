8574203573c71bcbbba001af11c928d0fc7bffed
jack <jack.xu.nz@gmail.com>
2019-12-19 22:38:36 +0000

    login page build session controller

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -3,7 +3,7 @@
         <a class="navbar-brand" href="{{route('home')}}">DATA App</a>
         <ul class="navbar-nav justify-content-end">
             <li class="nav-item"><a class="nav-link" href="{{route('help')}}">HELP</a></li>
-            <li class="nav-item" ><a class="nav-link" href="#">LOGIN</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{route('login')}}">LOGIN</a></li>
         </ul>
     </div>
 </nav>
