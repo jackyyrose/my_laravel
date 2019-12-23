d38e82ca0c51c599939919ee1368c732efa8c823
jack <jack.xu.nz@gmail.com>
2019-12-23 19:56:41 +0000

    toggle nav bar after success login; data-toggle=dropdown

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Auth;
 
 class UsersController extends Controller
 {
@@ -32,6 +33,7 @@ class UsersController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
+        Auth::login($user);
         session()->flash('success', 'Great welcome, a new start from here~');
         return redirect()->route('users.show', [$user]);
     }
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,8 +2,30 @@
     <div class="container ">
         <a class="navbar-brand" href="{{route('home')}}">DATA App</a>
         <ul class="navbar-nav justify-content-end">
+            @if (Auth::check())
+            <li class="nav-item"><a class="nav-link" href="#">User List</a></li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">
+                    {{ Auth::user()->name }}
+                </a>
+
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('users.show', Auth::user()) }}">Personal Center</a>
+                    <a class="dropdown-item" href="#">Edit</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" id="logout" href="#">
+                        <form action="{{ route('logout') }}" method="POST">
+                            {{ csrf_field() }}
+                            {{ method_field('DELETE') }}
+                            <button class="btn btn-block btn-danger" type="submit" name="button">Logout</button>
+                        </form>
+                    </a>
+                </div>
+            </li>
+            @else
             <li class="nav-item"><a class="nav-link" href="{{route('help')}}">HELP</a></li>
             <li class="nav-item"><a class="nav-link" href="{{route('login')}}">LOGIN</a></li>
+            @endif
         </ul>
     </div>
 </nav>
