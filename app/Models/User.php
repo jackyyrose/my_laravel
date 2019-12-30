b3a7c73d073a692db5900d6131c0d1c5a91669d0
jack <jack.xu.nz@gmail.com>
2019-12-30 21:09:19 +0000

    followers table and attach-sync-detach test

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,4 +60,40 @@ class User extends Authenticatable
         return $this->statuses()
             ->orderBy('created_at', 'desc');
     }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+        //关联关系表名称是user_user，指定为参数2，参数3是关联中的model的外键，参数4是合并的模型的外键
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+        //sync 方法会接收两个参数，第一个参数为要进行
+        //添加的 id ，第二个参数则指明是否要移除其它不包含在关联的 id 数组中的 id ， true 表
+        //示移除， false 表示不移除，默认值为 true 。由于我们在关注一个新用户的时候，仍然要保持之
+        //前已关注用户的关注关系，因此不能对其进行移除，
+    }
+
+    public function unfollow($user_id)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
 }
