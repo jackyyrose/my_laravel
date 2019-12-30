335bed39dab0c528341ad0f56c3ea7c57be92977
jack <jack.xu.nz@gmail.com>
2019-12-30 01:20:01 +0000

    blog data(statuses) model build

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,9 @@ class User extends Authenticatable
             $user->activation_token = str_random(30);
         });
     }
+
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
 }
